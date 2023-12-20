f624ed8ab71c652ff3ed3938fdf054225949481d
Moonsoul <xdfrse@gmail.com>
2023-12-20 19:30:02 +0200

    Remove unused compare function

--- hw11/arr.hpp
+++ hw11/arr.hpp
@@ -3,5 +3,4 @@
 #include "sorting_direction.hpp"
 
 void printArray(const int* arr, int size);
-bool compare(int a, int b, SortingDirection direction);
 bool isSorted(const int* arr, int size, SortingDirection direction);
